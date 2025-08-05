a388009c35e84231d6921338fec9820c7a74e5d1
deepak.yenagandula <deepak.yenagandula@tectoro.com>
2025-08-05 12:03:45 +0530

    added comments for async and await

--- ConsoleApp1/MultiThreading/AsyncAndAwait.cs
+++ ConsoleApp1/MultiThreading/AsyncAndAwait.cs
@@ -9,15 +9,30 @@ public class AsyncAndAwait
 
 
 
-    static void Main()
+    static async Task Main()
     {
+        Console.WriteLine("Main thread started execution...");
+        // Here main thread will comeback and prints Main thread execution completed... immediately  because the task is not awaited so it runs in background
+        // If we awaits the task like this while calling await DoHeavyTask(); then the main thread will not move forward until the DoHeavyTask is completed.
+        // or else we can start and we can await it whenever we want the value or completion of task like await t;
         Task t = DoHeavyTask();
-        
+        Console.WriteLine("Main thread execution completed...");
     }
 
+    // When main thread calls this method and reaches await keyword then main thread will return to Main method continues its execution without waiting for this DoHeavyTask Method
+    // this method will be runned in background and returns a Task obj to main method
     static async Task DoHeavyTask()
     {
-        await Task.Delay(5000);
+        await RunCPUBoundWork();
         Console.WriteLine("heavy task completed");
+    }
+
+
+    static async Task RunCPUBoundWork()
+    {
+        for (int i = 0; i < 5000; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
